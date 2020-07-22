98763a3c405dd51164ba8670e309c0317353fc7b
davidpodhola <david.podhola@naseukoly.cz>
2020-07-22 10:17:34 +0200

    UI spec tests too

--- libs/ui/src/lib/ui.spec.tsx
+++ libs/ui/src/lib/ui.spec.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import { render } from '@testing-library/react';
 
 import Ui from './ui';
+import App from '../../../../apps/web/src/app/app';
 
 describe(' Ui', () => {
   it('should render successfully', () => {
     const { baseElement } = render(<Ui />);
     expect(baseElement).toBeTruthy();
   });
+
+  it('should have a greeting as the title', () => {
+    const { getByText } = render(<Ui />);
+
+    expect(getByText('Welcome to ui!')).toBeTruthy();
+  });
+
 });

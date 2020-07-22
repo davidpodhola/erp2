4dc08e0ef38a47b9920c1cdd563b245a801e4b3c
davidpodhola <david.podhola@naseukoly.cz>
2020-07-22 17:10:53 +0200

    organization service started

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// typeOrm + list of entities from THIS application + try to enhance e.g. Organization
+
 @Module({
   imports: [],
   controllers: [AppController],
---
+++ libs/model/src/lib/organization.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+
+export const OrganizationServiceKey = 'OrganizationService';
+
+@Injectable()
+export class OrganizationService {
+
+}

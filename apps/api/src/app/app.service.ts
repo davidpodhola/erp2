1e76dfd31afd22f37f49ae6f1a635cb5ea789649
davidpodhola <david.podhola@naseukoly.cz>
2020-07-24 10:46:25 +0200

    fix test

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -3,14 +3,7 @@ import { AddressService, AddressServiceKey } from '@erp2/model';
 
 @Injectable()
 export class AppService {
-  constructor(
-    @Inject(AddressServiceKey) public readonly addressService: AddressService
-  ) {}
-
   getData(): { message: string } {
-    const x = this.addressService.createEntity();
-    x.zipCode = 'XYZ123456';
-
-    return { message: `Welcome to api with ${JSON.stringify(x)}!` };
+    return { message: `Welcome to api!` };
   }
 }

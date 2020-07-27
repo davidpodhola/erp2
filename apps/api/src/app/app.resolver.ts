a98cd2cb65201acc9c6946a0e538f96df5a5c802
davidpodhola <david.podhola@naseukoly.cz>
2020-07-27 14:55:59 +0200

    server graphql works

---
+++ apps/api/src/app/app.resolver.ts
@@ -0,0 +1,11 @@
+import { Query, Resolver } from '@nestjs/graphql';
+import { DateTimeScalarType } from './support/date.scalar';
+
+
+@Resolver()
+export class AppResolver {
+  @Query(() => DateTimeScalarType)
+  async now(): Promise<Date> {
+    return new Date();
+  }
+}
---
+++ apps/api/src/app/resolvers.ts
@@ -0,0 +1,5 @@
+import { AppResolver } from './app.resolver';
+
+export const resolvers = [
+  AppResolver,
+];

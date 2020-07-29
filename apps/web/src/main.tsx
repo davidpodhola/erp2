e696f433bc6bd499d9044837b974239c3a773f08
davidpodhola <david.podhola@naseukoly.cz>
2020-07-29 21:31:41 +0200

    use GlobalStyles (=clarity.desing) also in the storybook

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -4,11 +4,13 @@ import ReactDOM from 'react-dom';
 import App from './app/app';
 import { ApolloProvider } from '@apollo/client';
 import { client } from './client';
+import { GlobalStyles } from '@erp2/ui';
 
 ReactDOM.render(
   <ApolloProvider client={client}>
     <React.StrictMode>
       <App />
+      <GlobalStyles />
     </React.StrictMode>
   </ApolloProvider>,
   document.getElementById('root')

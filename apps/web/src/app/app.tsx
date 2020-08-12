7d1314328480cfda521598b356737ab726bad97d
davidpodhola <david.podhola@naseukoly.cz>
2020-08-12 06:34:41 +0200

    can login and get token

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Ui, ServerTime } from '@erp2/ui';
 import { useAuth0 } from '@auth0/auth0-react';
 
 export const App = () => {
-  const { loginWithRedirect, isAuthenticated, isLoading } = useAuth0();
+  const {
+    loginWithRedirect,
+    isAuthenticated,
+    isLoading,
+    getAccessTokenSilently,
+  } = useAuth0();
   // if (!isAuthenticated) loginWithRedirect();
+  const [token, setToken] = useState<string>(null);
+  useEffect(() => {
+    const setTokenIfAuthenticated = async () => {
+      if (isAuthenticated) setToken(await getAccessTokenSilently());
+    };
 
-  return /*(isLoading || !isAuthenticated ) ? (<div>Loading...</div>) :*/ (
-    <div className="main-container">
+    setTokenIfAuthenticated().then();
+  }, [isAuthenticated, getAccessTokenSilently]);
+
+  return (
+    /*(isLoading || !isAuthenticated ) ? (<div>Loading...</div>) :*/ <div className="main-container">
       <div className="alert alert-app-level">Alert</div>
       <header className="header header-6">Header</header>
       <nav className="subnav">subnav</nav>
@@ -16,9 +29,11 @@ export const App = () => {
         <div className="content-area">
           <Ui />
           <ServerTime />
-          <div
-            onClick={() => loginWithRedirect()}
-          >Login...</div>
+          {token ? (
+            <span>Token: ${token}</span>
+          ) : (
+            <div onClick={() => loginWithRedirect()}>Login...</div>
+          )}
         </div>
         <nav className="sidenav">sidenav</nav>
       </div>
--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -10,7 +10,12 @@ import { Auth0Provider } from '@auth0/auth0-react';
 ReactDOM.render(
   <ApolloProvider client={client}>
     <React.StrictMode>
-      <Auth0Provider domain={process.env.NX_AUTH0_DOMAIN} clientId={process.env.NX_AUTH0_CLIENTID} audience={process.env.NX_AUTH0_AUDIENCE} redirectUri={'http://localhost:4200'}>
+      <Auth0Provider
+        domain={process.env.NX_AUTH0_DOMAIN}
+        clientId={process.env.NX_AUTH0_CLIENTID}
+        audience={process.env.NX_AUTH0_AUDIENCE}
+        redirectUri={'http://localhost:4200'}
+      >
         <App />
       </Auth0Provider>
       <GlobalStyles />

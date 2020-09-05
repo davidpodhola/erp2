878228a923da5e53a2a6ebe6f7c9f8705005f426
davidpodhola <david@podhola.net>
2020-09-05 05:57:04 +0200

    invo

--- apps/api/src/app/migrations.ts
+++ apps/api/src/app/migrations.ts
@@ -4,6 +4,7 @@ import { CreateInvoice202007B1596628283384 } from './migrations/1596628283384-Cr
 import { CreateInvoice202007C1597723044723 } from './migrations/1597723044723-CreateInvoice202007C';
 import { Invoices2020081598520059145 } from './migrations/1598520059145-Invoices202008';
 import { Invoices202008B1599053746373 } from './migrations/1599053746373-Invoices202008B';
+import { Invoices202008C1599277217737 } from './migrations/1599277217737-Invoices202008C';
 
 export const migrations = [
   CreateAnAddress1595508635328,
@@ -12,4 +13,5 @@ export const migrations = [
   CreateInvoice202007C1597723044723,
   Invoices2020081598520059145,
   Invoices202008B1599053746373,
+  Invoices202008C1599277217737,
 ];

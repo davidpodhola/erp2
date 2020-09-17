6900ebe0c055d6145706e4dc72be76ab232d96a5
davidpodhola <david@podhola.net>
2020-09-17 21:24:20 +0200

    WIP mobile

--- libs/ui/src/components/CustomersList.tsx
+++ libs/ui/src/components/CustomersList.tsx
@@ -15,26 +15,16 @@ const useStyles = makeStyles({
   },
 });
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
-const GET_DOGS = gql`
+const GET_CUSTOMERS = gql`
   query { customers { id idNumber legalName legalAddress {id} } }
 `;
 
 export const CustomersList = (args) => {
   const classes = useStyles();
 
-  const { loading, error, data } = useQuery(GET_DOGS);
+  const { loading, error, data } = useQuery(GET_CUSTOMERS);
+  const { customers } = data || { customers: [] };
+  console.log(`**** customers`, data);
 
   if (loading) return (<span>Loading...</span>);
   if (error) return (<span>`Error! ${error.message}`</span>);
@@ -44,7 +34,7 @@ export const CustomersList = (args) => {
       <Table className={classes.table} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell>#</TableCell>
             <TableCell align="right">Calories</TableCell>
             <TableCell align="right">Fat&nbsp;(g)</TableCell>
             <TableCell align="right">Carbs&nbsp;(g)</TableCell>
@@ -52,15 +42,15 @@ export const CustomersList = (args) => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.name}>
+          {customers.map((row) => (
+            <TableRow key={row.id}>
               <TableCell component="th" scope="row">
-                {row.name}
+                {row.id}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="right">{row.legalName}</TableCell>
+              <TableCell align="right">{row.idNumber}</TableCell>
+              <TableCell align="right">{row.legalAddress.id}</TableCell>
+              <TableCell align="right">{row.legalName}</TableCell>
             </TableRow>
           ))}
         </TableBody>
